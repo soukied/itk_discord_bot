d5aabfbe79dd1804bae890301e1015ce6dd78891
Adhya Adam Sulthan <admajor.redstoner@gmail.com>
2019-09-19 20:46:55 +0800

    update

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 var Discord = new require("discord.js");
 var client = new Discord.Client();
 
+function runCommand(ev) {
+	
+}
+
 client.on("ready", function() {
 	console.log("Bot is ready");
 });
 
 client.on("message", function(msg) {
-	if (msg.content == "ping") msg.channel.send("pong");
+	var message = msg.content.trim();
+	if (message.length > 1 && message[0] == ".") runCommand({command:message.slice(1),event:msg});
 });
 
-client.login("NjI0MDU3MDc3OTAzMzkyNzY4.XYN05A.KJWtg4vTy9DSdkxOa6pXAoicnUQ");
+client.login("BOT_TOKEN");
