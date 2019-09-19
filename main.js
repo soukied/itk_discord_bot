2a8cfa73af152d9675a240c9732f9c8cb3754bee
Adhya Adam Sulthan <admajor.redstoner@gmail.com>
2019-09-19 21:12:02 +0800

    update

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var Discord = new require("discord.js");
 var client = new Discord.Client();
 
+var bot_key;
+
+if (process.argv[2] == "hidden") bot_key = "BOT_KEY";
+else bot_key = "NjI0MDU3MDc3OTAzMzkyNzY4.XYN72g.QwNBcMlPH6wx1Wpnm0jZdsFaLmo";
+
 function runCommand(ev) {
 	
 }
