01a27d8588c47b197e6bce8a7971504fd9263d52
Adhya Adam Sulthan <admajor.redstoner@gmail.com>
2019-09-27 01:47:25 +0800

    update

--- command.js
+++ command.js
@@ -77,7 +77,7 @@ addCommand(
 		for (var i = 0; i < args.length; i++) {
 			var nuclearCode = args[i];
 			nhentai.exists(nuclearCode).then(function(isExist) {
-				if (isExist) ev.channel.send(mentionMember(ev.author) + ", kode `" + nuclearCode + "` tersedia. :white_check_mark:");
+				if (isExist) ev.channel.send("> "+mentionMember(ev.author) + "\n> kode `" + nuclearCode + "` tersedia. :white_check_mark:\n> URL : https://nhentai.net/g/" + nuclearCode);
 				else ev.channel.send(mentionMember(ev.author) + ", kode `" + nuclearCode + "` tidak tersedia. :negative_squared_cross_mark:");
 			});
 
